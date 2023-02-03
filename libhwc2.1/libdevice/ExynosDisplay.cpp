slogar25/aosp_hardware_google_graphics_common

libhwc2.1: avoid redundant wait while enabling LHBM
We may have extra wait for the peak refresh while enabling LHBM
if smooth display is disabled. Use condition wait instead of
vblank so that we can continue enabling LHBM after receiving the
peak notification.

Bug: 264719811
Test: capture trace
Change-Id: Ie96aa21f1e0353578bb34c0655297700766ec5ed

libhwc2.1/libdevice/ExynosDisplay.cpp
@@ -1021,7 +1021,7 @@ ExynosDisplay::ExynosDisplay(uint32_t type, uint32_t index, ExynosDevice *device
         mSkipFrame(false),
         mVsyncPeriodChangeConstraints{systemTime(SYSTEM_TIME_MONOTONIC), 0},
         mVsyncAppliedTimeLine{false, 0, systemTime(SYSTEM_TIME_MONOTONIC)},
-        mConfigRequestState(hwc_request_state_t::SET_CONFIG_STATE_NONE),
+        mConfigRequestState(hwc_request_state_t::SET_CONFIG_STATE_DONE),
         mPowerHalHint(mDisplayId, mDisplayTraceName),
         mErrLogFileWriter(2, ERR_LOG_SIZE),
         mDebugDumpFileWriter(10, 1, ".dump"),
@@ -4253,10 +4253,18 @@ int32_t ExynosDisplay::resetConfigRequestStateLocked(hwc2_config_t config) {
         DISPLAY_LOGI("%s: mConfigRequestState (%d) is not REQUESTED", __func__,
                      mConfigRequestState);
     } else {
-        DISPLAY_LOGD(eDebugDisplayInterfaceConfig, "%s: Change mConfigRequestState (%d) to NONE",
+        DISPLAY_LOGD(eDebugDisplayInterfaceConfig, "%s: Change mConfigRequestState (%d) to DONE",
                      __func__, mConfigRequestState);
-        mConfigRequestState = hwc_request_state_t::SET_CONFIG_STATE_NONE;
+        mConfigRequestState = hwc_request_state_t::SET_CONFIG_STATE_DONE;
         updateAppliedActiveConfig(mActiveConfig, systemTime(SYSTEM_TIME_MONOTONIC));
+
+        std::lock_guard<std::mutex> lock(mPeakRefreshRateMutex);
+        bool isPeakRefreshRate = isCurrentPeakRefreshRate();
+        DISPLAY_ATRACE_INT("isPeakRefreshRate", isPeakRefreshRate);
+        if (isPeakRefreshRate && mNotifyPeakRefreshRate) {
+            mPeakRefreshRateCondition.notify_one();
+            mNotifyPeakRefreshRate = false;
+        }
     }
     return NO_ERROR;
 }

msft-mirror-aosp/platform.hardware.google.graphics.common

Bug: 263821118
Merged-In: Ie96aa21f1e0353578bb34c0655297700766ec5ed
(cherry picked from commit 376a8b2db4b871d224c21b6f3da475b0336bd7cf)

libhwc2.1/libdevice/ExynosDisplay.cpp
@@ -1000,7 +1000,7 @@ ExynosDisplay::ExynosDisplay(uint32_t index, ExynosDevice *device)
         mSkipFrame(false),
         mVsyncPeriodChangeConstraints{systemTime(SYSTEM_TIME_MONOTONIC), 0},
         mVsyncAppliedTimeLine{false, 0, systemTime(SYSTEM_TIME_MONOTONIC)},
-        mConfigRequestState(hwc_request_state_t::SET_CONFIG_STATE_NONE),
+        mConfigRequestState(hwc_request_state_t::SET_CONFIG_STATE_DONE),
         mPowerHalHint(getDisplayId(mDisplayId, mIndex)) {
     mDisplayControl.enableCompositionCrop = true;
     mDisplayControl.enableExynosCompositionOptimization = true;
@@ -4247,10 +4247,18 @@ int32_t ExynosDisplay::resetConfigRequestStateLocked(hwc2_config_t config) {
         DISPLAY_LOGI("%s: mConfigRequestState (%d) is not REQUESTED", __func__,
                      mConfigRequestState);
     } else {
-        DISPLAY_LOGD(eDebugDisplayInterfaceConfig, "%s: Change mConfigRequestState (%d) to NONE",
+        DISPLAY_LOGD(eDebugDisplayInterfaceConfig, "%s: Change mConfigRequestState (%d) to DONE",
                      __func__, mConfigRequestState);
-        mConfigRequestState = hwc_request_state_t::SET_CONFIG_STATE_NONE;
+        mConfigRequestState = hwc_request_state_t::SET_CONFIG_STATE_DONE;
         updateAppliedActiveConfig(mActiveConfig, systemTime(SYSTEM_TIME_MONOTONIC));
+
+        std::lock_guard<std::mutex> lock(mPeakRefreshRateMutex);
+        bool isPeakRefreshRate = isCurrentPeakRefreshRate();
+        ATRACE_INT("isPeakRefreshRate", isPeakRefreshRate);
+        if (isPeakRefreshRate && mNotifyPeakRefreshRate) {
+            mPeakRefreshRateCondition.notify_one();
+            mNotifyPeakRefreshRate = false;
+        }
     }
     return NO_ERROR;
 }