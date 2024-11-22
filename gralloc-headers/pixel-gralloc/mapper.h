9fb48c8acc47bc80661cabb860105bd4dc497256
Devika Krishnadas <kdevika@google.com>
2024-11-22 03:21:20 +0000

    Remove mapper_version soong variable dependency
    This header is expected to be used by gralloc and device agnostic tests.
    This means that there should be no dependency on gralloc implementation
    related libraries, headers or defaults here.
    
    Bug: 380471606
    Test: atest GrallocExperimentalFeatures#TestMapper
          atest gfx-gralloc5-experimental-features-test (on WHI devices)
    Change-Id: Ie121775e489606788e6839e25f6e8d9a0ecac5d9
    Signed-off-by: Devika Krishnadas <kdevika@google.com>

--- gralloc-headers/pixel-gralloc/mapper.h
+++ gralloc-headers/pixel-gralloc/mapper.h
@@ -1,9 +1,33 @@
 #pragma once
 
-#if defined(MAPPER_5)
-#include "mapper5.h"
-#elif defined(MAPPER_4)
 #include "mapper4.h"
-#else
-#error "Mapper not found"
-#endif
+#include "mapper5.h"
+
+namespace pixel::graphics::mapper {
+
+// TODO: b/384593969: Fix mapper5 selection based on allocator version
+template <MetadataType meta>
+std::optional<typename metadata::ReturnType<meta>::type> get(buffer_handle_t handle) {
+    static bool use_v5 = [&handle]() {
+        return v5::get<MetadataType::PIXEL_FORMAT_ALLOCATED>(handle).has_value();
+    }();
+
+    if (use_v5) {
+        return v5::get<meta>(handle);
+    }
+    return v4::get<meta>(handle);
+}
+
+template <MetadataType meta>
+int32_t set(buffer_handle_t handle, typename metadata::ReturnType<meta>::type data) {
+    // Using v5::get() just to set the boolean
+    static bool use_v5 = [&handle]() {
+        return v5::get<MetadataType::PIXEL_FORMAT_ALLOCATED>(handle).has_value();
+    }();
+
+    if (use_v5) {
+        return v5::set<meta>(handle, data);
+    }
+    return v4::set<meta>(handle, data);
+}
+} // namespace pixel::graphics::mapper
